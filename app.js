628df86a04584c9c4bf98335637f91de522f67eb
Alfredo Poveda <alfpovsistemas@gmail.com>
2020-03-18 01:09:03 -0400

    Se agrego algo de soporte para swagger, pero se removera para seguir con el tutorial

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./api/openapi.json");
 
 const app = express();
+const port = process.env.PORT || 5000;
+
+const swaggerOptions = {
+  swaggerDefinition: swaggerDocument,
+  apis: ["app.js"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(null, options));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const places = [
   {
@@ -25,12 +43,47 @@ const places = [
   }
 ];
 
+// Routes
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    tags: ["home"]
+ *    summary: "Update an existing pet"
+ *    operationId: "updatePet"
+ *    requestBody:
+ *      description: "Use to request all places"
+ *      content:
+ *        "application/json":
+ *          schema:
+ *            "$ref": "#/components/schemas/Pet"
+ *      required: true
+ *    responses:
+ *      "200":
+ *        description: "A successful response"
+ *      "400":
+ *        description: "Invalid ID supplied"
+ *        content:
+ *      "404":
+ *        description: "Pet not found"
+ *        content:
+ *      "405":
+ *        description: "Validation exception"
+ *        content:
+ *    security:
+ *      petstore_auth: ["write:pets", "read:pets"]
+ *    "x-codegen-request-body-name": "body"
+ */
 app.get("/", (req, res) => {
   res.json(places);
 });
 
+app.post("/", (req, res) => {
+  res.json(req.body);
+});
+
 app.use(express.static("public"));
 
-app.listen(3000, () => {
-  console.log("Estoy listo para escuchar peticiones");
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
