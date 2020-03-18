fafd037c44809ea65caf9973985ad462f823d94e
Alfredo Poveda <alfpovsistemas@gmail.com>
2020-03-18 01:10:48 -0400

    Se agrego algo de soporte para swagger, este seria un punto inicil para comenzar un proyecto, pero se removera para seguir con el tutorial

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./api/openapi.json");
+// const swaggerJsDoc = require("swagger-jsdoc");
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerDocument = require("./api/openapi.json");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-const swaggerOptions = {
-  swaggerDefinition: swaggerDocument,
-  apis: ["app.js"]
-};
+// const swaggerOptions = {
+//   swaggerDefinition: swaggerDocument,
+//   apis: ["app.js"]
+// };
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
+// const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(null, options));
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({ extended: false }));
 
