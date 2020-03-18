e94744389ceae9a7ec11c3ddfca8de61a76b7048
Alfredo Poveda <alfpovsistemas@gmail.com>
2020-03-18 01:12:30 -0400

    Se agrego algo de soporte para swagger

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const swaggerJsDoc = require("swagger-jsdoc");
-// const swaggerUi = require("swagger-ui-express");
-// const swaggerDocument = require("./api/openapi.json");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// const swaggerOptions = {
-//   swaggerDefinition: swaggerDocument,
-//   apis: ["app.js"]
-// };
-
-// const swaggerDocs = swaggerJsDoc(swaggerOptions);
-
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -41,37 +30,6 @@ const places = [
   }
 ];
 
-// Routes
-/**
- * @swagger
- * /:
- *  get:
- *    tags: ["home"]
- *    summary: "Update an existing pet"
- *    operationId: "updatePet"
- *    requestBody:
- *      description: "Use to request all places"
- *      content:
- *        "application/json":
- *          schema:
- *            "$ref": "#/components/schemas/Pet"
- *      required: true
- *    responses:
- *      "200":
- *        description: "A successful response"
- *      "400":
- *        description: "Invalid ID supplied"
- *        content:
- *      "404":
- *        description: "Pet not found"
- *        content:
- *      "405":
- *        description: "Validation exception"
- *        content:
- *    security:
- *      petstore_auth: ["write:pets", "read:pets"]
- *    "x-codegen-request-body-name": "body"
- */
 app.get("/", (req, res) => {
   res.json(places);
 });
