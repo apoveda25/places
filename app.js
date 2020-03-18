68471dea6eaa03e8f8340e26744658c8d8c1c7ea
Alfredo Poveda <alfpovsistemas@gmail.com>
2020-03-17 22:33:21 -0400

    Servidor de archivos estaticos

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
   res.send("Hola mundo");
 });
 
+app.use(express.static("public"));
+
 app.listen(3000, () => {
   console.log("Estoy listo para escuchar peticiones");
 });
